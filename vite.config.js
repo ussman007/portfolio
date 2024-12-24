117dc9e18c0a5e7a9980b083408e2aadd5e684d0
ussman007 <musmn007@gmail.com>
2024-12-24 22:40:01 +0500

    mmjj

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,21 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+            },
+        },
     },
-  },
-  server: {
-    port: 3000
-  }
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        port: 3000
+    }
 }) 
